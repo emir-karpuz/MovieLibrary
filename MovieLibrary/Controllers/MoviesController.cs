d59267713d1e16984adccd40c42fe7bfe4d71976
Emir <gsgtrascal@live.com>
2021-05-08 15:34:53 +0300

    Test amaçlı route, controller ve action'lar eklendi.

---
+++ MovieLibrary/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using MovieLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieLibrary.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { ID = 1, Name = "John Wick"};
+            return View(movie);
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
